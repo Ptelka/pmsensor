c92ed5eeddb1073aeb2f71f46a83c56d585aad6f
Piotr Telka <piotr.telka1@gmail.com>
2019-12-18 20:40:56 +0100

    x

--- prepare.sh
+++ prepare.sh
@@ -3,3 +3,5 @@
 sudo apt-get update
 sudo apt-get install python3-pip python3-scipy python3-seaborn sqlite3 -y
 pip3 install pandas
+
+sqlite3 measures.db < create.sql
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for i in {1..500}
+for i in {1..5000}
 do
-  python3 ./get-measures.py
+  python3 ./get-measures.py || true
+  python3 ./refresh.py || true
   echo ${i}
-  sleep 20m
+  sleep 10s
 done
